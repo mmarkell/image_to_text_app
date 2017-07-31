72c2b7b653be156ac1116e6505cd6b5b16aed1a2
mm116 <michael_markell@brown.edu>
2017-07-31 12:19:41 -0700

    it is working

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ var logger = require('morgan');
 var exphbs = require('express-handlebars');
 var expstate = require('express-state');
 var request = require('request');
-var _ = require('lomath');
+var _ = require('lodash');
 var app = express();
 var PORT = 3000;
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var multer  = require('multer');
+var upload = multer({ dest: 'temp/images'});
+var fs = require('fs');
+
+/** Permissible loading a single file, 
+    the value of the attribute "name" in the form of "recfile". **/
+var type = upload.single('imageUpload');
+var path = require('path');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -17,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use('/public', express.static('public'));
+app.use(multer({dest:'./temp/images/'}).single('imageUpload'));
 expstate.extend(app);
 
 app.set('state namespace', 'App');
@@ -30,25 +39,67 @@ app.get("/", function(req, res) {
     res.render('upload');
 });
 
-function post(res, url, form) {
+app.post("/", function(req, res) {
+    var imageFile = fs.readFileSync('temp/images/image.png');
+    var encoded = new Buffer(imageFile).toString('base64');
+    postAnnotation(res, 'https://vision.googleapis.com/v1/images:annotate', { "requests":[{ "features":[{"type": "LABEL_DETECTION"}, {"maxResults": 10} ],"image":{"content":encoded}}]});
+});
+
+app.post("/upload", function(req, res) {
+    if (req.file != undefined) {
+        var tempPath = req.file.path;
+        var target_path = 'temp/images/image.png';
+        var src = fs.createReadStream(tempPath);
+        var dest = fs.createWriteStream(target_path);
+        src.pipe(dest);
+        res.render('upload');
+    }
+    else {
+        console.log("failed");
+        res.render('upload');
+    }
+});
+
+app.get("/image.png", function (req, res) {
+    res.sendFile(path.resolve('./temp/images/image.png'));
+}); 
+
+function postAnnotation(res, url, form) {
     url = url + '?key=' + authToken;
     x = request.post(url, { json: form }, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            var str = JSON.stringify(body.responses[0].landmarkAnnotations[0].description
-, null, 2); 
-            console.log(str);
-            res.render('upload', {bodies: str});
+            var descriptions = getDescriptions(body, 'labelAnnotations');
+            res.render('upload', {bodies: descriptions});
+            return;
         }
         else {
-            console.log('hi');
+            console.log(error);
+            console.log(response.statusCode);
+            res.render('upload');
             return error;
         }
     });
 }
 
-app.post("/", function(req, res) {
-    post(res, 'https://vision.googleapis.com/v1/images:annotate', { "requests":[{ "features":[{"type": "LANDMARK_DETECTION"}, {"maxResults": 10} ],"image":{"source":{"gcsImageUri": "gs://snappy-dragon-3843/DSCF8969.jpg"}}}]});
-});
+function getDescriptions(body, feature) {
+    jBody = body.responses
+    jsonDescriptions = "";
+    for (var i = 0; i < jBody.length; i++){
+        for (var key in jBody[i]){
+            var attrName = key;
+            if (attrName === feature) {
+                for (var key2 in jBody[i][key]) {
+                    // var attrName = jBody[i][key][key2];
+                    // jsonDescriptions.push({
+                    //     id: 'tag',
+                    // });
+                    jsonDescriptions += " | " + jBody[i][key][key2].description;
+                }
+            }
+        } 
+    }
+    return jsonDescriptions;
+}
 
 http.listen(PORT, function() {
     console.log('Example app listening on port 3000!');
